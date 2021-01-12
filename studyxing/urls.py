db051767049e409bf6b0ea5895d8b82d86f5c9a3
cuijianzhe <598941324@qq.com>
2021-01-12 19:14:22 +0800

    url改变1

--- studyxing/urls.py
+++ studyxing/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import path
 from django.urls import include
 urlpatterns = [
-    path('api/v1/',include({
-        path('account/',include('account.urls')),
+    path('api/v1/', include({
+        path('account/', include('account.urls')),
     }))
 ]
