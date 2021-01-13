c46e00fba471f77df1c967c861a3ff8f69228b95
cuijianzhe <598941324@qq.com>
2021-01-13 16:26:45 +0800

    增加部门模块信息

--- account/urls/__init__.py
+++ account/urls/__init__.py
@@ -1,7 +1,9 @@
 from account.urls import user
 from account.urls import mod
 from account.urls import permission
+from account.urls import department
 
 urlpatterns = user.urlpatterns + \
               mod.urlpatterns + \
-              permission.urlpatterns
+              permission.urlpatterns + \
+              department.urlpatterns
