da821084d56a29c9fe1d0ad3b0f1580c96af47d3
cuijianzhe <598941324@qq.com>
2021-01-13 09:37:50 +0800

    __init__添加引入

--- account/apis/user.py
+++ account/apis/user.py
@@ -12,8 +12,8 @@ class LoginApi(BaseApi):
     NEED_LOGIN = False
 
     need_params = {
-        'username':('用户名':'required str 32'),
-        'password':('密码':'required str 32'),
+        'username':('用户名','required str 32'),
+        'password':('密码','required str 32'),
         'is_ldap':('是否LDAP登录','required bool'),
     }
     def post(self,request,params):
