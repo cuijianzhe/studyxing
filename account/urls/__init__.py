8de358f6a31a9a3531c3626529637ccb80e27428
cuijianzhe <598941324@qq.com>
2021-01-16 01:40:07 -0500

    卧槽，好多抄的都不对，这里开始正常了吗？

--- account/urls/__init__.py
+++ account/urls/__init__.py
@@ -1,11 +1,12 @@
 from account.urls import user
-from account.urls import mod
-from account.urls import permission
-from account.urls import department
 from account.urls import role
+from account.urls import mod
+from account.urls import department
+from account.urls import permission
+
 
 urlpatterns = user.urlpatterns + \
+              role.urlpatterns + \
               mod.urlpatterns + \
-              permission.urlpatterns + \
               department.urlpatterns + \
-              role.urlpatterns
+              permission.urlpatterns
